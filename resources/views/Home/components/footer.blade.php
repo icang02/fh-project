f18e7bbc91d5b71eb4520faf5c9e1bc42bcce98c
Amharrayadin <amharrayadin@gmail.com>
2022-11-10 11:24:56 +0800

    footer

--- resources/views/Home/components/footer.blade.php
+++ resources/views/Home/components/footer.blade.php
@@ -1,55 +1,32 @@
 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
   <div class="container py-5">
     <div class="row g-5">
-      <div class="col-lg-3 col-md-6">
-        <h4 class="text-white mb-3">Quick Link</h4>
-        <a class="btn btn-link" href="">About Us</a>
-        <a class="btn btn-link" href="">Contact Us</a>
-        <a class="btn btn-link" href="">Privacy Policy</a>
-        <a class="btn btn-link" href="">Terms & Condition</a>
-        <a class="btn btn-link" href="">FAQs & Help</a>
+      <div class="col-lg-4 col-md-6">
+            <div class="mb-3">
+              <center>
+                <img src="{{ asset('home-assets/img/logo-uho.png') }}" width="150" class = "mx-auto" alt="Universitas Haluoleo">
+              </center>
+            </div>
+            <h5 class="text-white text-center mb-3">FAKULTAS HUKUM<br>UNIVERSITAS HALUOLEO</h5>
+      </div>    
+      <div class="col-lg-4 col-md-6">
+        <h5 class="text-white mb-3">TENTANG SITUS</h5>
+        <a class="btn btn-link" href="{{ url('/') }}">Beranda</a>
+        <a class="btn btn-link" href="{{ url('#') }}">Spada</a>
+        <a class="btn btn-link" href="{{ url('about') }}">Tentang</a>
+        <a class="btn btn-link" href="{{ url('#') }}">Hubungi Kami</a>
+        <a class="btn btn-link" href="{{ url('#') }}">Umpan Balik</a>
       </div>
-      <div class="col-lg-3 col-md-6">
-        <h4 class="text-white mb-3">Contact</h4>
-        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-        <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+      <div class="col-lg-4 col-md-6">
+        <h5 class="text-white mb-3">HUBUNGI KAMI</h5>
+        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Kampus Bumi Tridharma Jln. H.E.A Mokodompit, Anduonouhu Kendari, Sulawesi Tenggara 93232</p>
+        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>(0401) 3195217</p>
+        <p class="mb-2"><i class="fa fa-envelope me-3"></i>Fhukumuho@yahoo.co.id</p>
         <div class="d-flex pt-2">
-          <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-          <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-          <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-          <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6">
-        <h4 class="text-white mb-3">Gallery</h4>
-        <div class="row g-2 pt-2">
-          <div class="col-4">
-            <img class="img-fluid bg-light p-1" src="{{ asset('home-assets') }}/img/course-1.jpg" alt="">
-          </div>
-          <div class="col-4">
-            <img class="img-fluid bg-light p-1" src="{{ asset('home-assets') }}/img/course-2.jpg" alt="">
-          </div>
-          <div class="col-4">
-            <img class="img-fluid bg-light p-1" src="{{ asset('home-assets') }}/img/course-3.jpg" alt="">
-          </div>
-          <div class="col-4">
-            <img class="img-fluid bg-light p-1" src="{{ asset('home-assets') }}/img/course-2.jpg" alt="">
-          </div>
-          <div class="col-4">
-            <img class="img-fluid bg-light p-1" src="{{ asset('home-assets') }}/img/course-3.jpg" alt="">
-          </div>
-          <div class="col-4">
-            <img class="img-fluid bg-light p-1" src="{{ asset('home-assets') }}/img/course-1.jpg" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6">
-        <h4 class="text-white mb-3">Newsletter</h4>
-        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-        <div class="position-relative mx-auto" style="max-width: 400px;">
-          <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-          <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+          <a class="btn btn-outline-light btn-social" href="{{url('https://web.facebook.com/groups/790598337644887/')}}"><i class="fab fa-facebook-f"></i></a>
+          <a class="btn btn-outline-light btn-social" href="{{url('https://www.instagram.com/fakultashukumuho/')}}"><i class="fab fa-instagram"></i></a>
+          <a class="btn btn-outline-light btn-social" href="{{url('https://www.youtube.com/channel/UCyaZXJNwkmtNyi8AQOrzrJw')}}"><i class="fab fa-youtube"></i></a>
+          <a class="btn btn-outline-light btn-social" href="{{url('mailto:Fhukumuho@yahoo.co.id')}}"><i class="fa fa-envelope"></i></a>
         </div>
       </div>
     </div>
@@ -58,15 +35,11 @@
     <div class="copyright">
       <div class="row">
         <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-          &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
-
-          <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-          Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a><br><br>
-          Distributed By <a class="border-bottom" href="https://themewagon.com">ThemeWagon</a>
+          &copy;Hak Cipta 2017-<?php echo date('Y'); ?> <a class="border-bottom" href="{{ url('/') }}"> Fakultas Hukum UHO</a>
         </div>
         <div class="col-md-6 text-center text-md-end">
           <div class="footer-menu">
-            <a href="">Home</a>
+            <a href="{{ url('/') }}">Home</a>
             <a href="">Cookies</a>
             <a href="">Help</a>
             <a href="">FQAs</a>
