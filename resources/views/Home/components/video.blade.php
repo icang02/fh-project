59ea9a764b30f3b983958ea04c2a8514bfb379aa
Amharrayadin <amharrayadin@gmail.com>
2022-11-13 01:54:26 +0800

    index visi

--- resources/views/Home/components/video.blade.php
+++ resources/views/Home/components/video.blade.php
@@ -1,4 +1,10 @@
 <div class="container">
+    <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+        <h6 class="section-title bg-white text-center text-primary px-3">Video Profil</h6>
+        <h1 class="mb-5">Fakultas Hukum UHO</h1>
+    </div>
+</div>
+<div class="container-fluid">
     <div class="videoWrapper"><iframe src="https://www.youtube.com/embed/V5ktqaoS1QE?rel=0" frameborder="0" allowfullscreen
             class="video"></iframe></div>.
 </div>
