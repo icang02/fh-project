55ee0fe5e0cfe216f9fcecec05590cb4b968b50d
Amharrayadin <amharrayadin@gmail.com>
2022-11-14 01:41:21 +0800

    kategori beritaa

--- resources/views/admin/berita/kategori-berita.blade.php
+++ resources/views/admin/berita/kategori-berita.blade.php
@@ -1,72 +1,32 @@
 @extends('layouts.admin')
 
 @section('main-content')
-    <div class="body flex-grow-1 px-3">
+    <div class="body px-3">
         <div class="container-lg">
             <div class="row">
-                <div style="float: left !important">
-                    <div class="d-inline">
-                        <div class="row">
-                            <div class="col col-lg-1">
-                                <select name="batas" id="batas" class="form-select">
-                                    <option value="5">5</option>
-                                    <option value="10">10</option>
-                                    <option value="25">25</option>
-                                    <option value="50">50</option>
-                                    <option value="100">100</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col col-lg-3">
-                            <div class="input-group d-inline">
-                                <form action="" method="POST">
-                                    <input type="text" name="keyword" id="keyword" class="form-control"
-                                        placeholder="Masukan Kata Kunci Pencarian">
-                                    <input type="submit" class="btn btn-primary" name="search" value="Cari">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col col-1">
+                    <select name="batas" id="batas" class="form-select">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
                 </div>
-                <div style="float: right">
-                    <div class="tab-content rounded-bottom">
-                        <div class="tab-pane p-3 active preview" role="tabpanel" id="preview-35">
-                            {{-- ISI MODAL TAMBAH --}}
-                            <div class="modal fade" id="modalAddKategori" tabindex="-1"
-                                aria-labelledby="modalAddKategoriLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="modalAddKategoriLabel">Tambah Kategori</h5>
-                                            <button class="btn-close" type="button" data-coreui-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <form action="" method="POST">
-                                            <div class="modal-body">
-                                                <div class="mb-3">
-                                                    <label for="kategori">Masukan Nama Kategori</label>
-                                                    <input type="text" name="kategori" id="kategori"
-                                                        class="form-control">
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button class="btn btn-secondary" type="button"
-                                                    data-coreui-dismiss="modal">Close</button>
-                                                <input type="submit" name="tambahkategori" class="btn btn-primary"
-                                                    value="Simpan">
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                            {{-- MODAL TAMBAH TRIGGER --}}
-                            <button class="btn btn-primary" type="button" data-coreui-toggle="modal"
-                                data-coreui-target="#modalAddKategori">Tambah</button>
-                        </div>
-                    </div>
+                <div class="col col-3" style="float: right !important">
+                    <input type="text" name="keyword" id="keyword" class="form-control"
+                        placeholder="Masukan Kata Kunci Pencarian">
+                </div>
+                <div class="col col-1">
+                    <input type="submit" style="float: left !important" class="btn btn-primary" name="search"
+                        value="Cari">
+                </div>
+                <div class="col col-7 align-right">
+                    <button class="btn btn-primary" style="float: right !important" type="button"
+                        data-coreui-toggle="modal" data-coreui-target="#modalAddKategori">Tambah</button>
                 </div>
             </div>
-            <div class="row">
+            <div class="row mt-3">
                 <div class="col">
                     <table class="table table-bordered table-hover">
                         <thead class="table-dark">
@@ -88,6 +48,32 @@
                             </tr>
                         </tbody>
                     </table>
+                    {{-- ISI MODAL TAMBAH --}}
+                    <div class="modal fade" id="modalAddKategori" tabindex="-1" aria-labelledby="modalAddKategoriLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalAddKategoriLabel">Tambah Kategori</h5>
+                                    <button class="btn-close" type="button" data-coreui-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <form action="" method="POST">
+                                    <div class="modal-body">
+                                        <div class="mb-3">
+                                            <label for="kategori">Masukan Nama Kategori</label>
+                                            <input type="text" name="kategori" id="kategori" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-secondary" type="button"
+                                            data-coreui-dismiss="modal">Close</button>
+                                        <input type="submit" name="tambahkategori" class="btn btn-primary" value="Simpan">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                     {{-- MODAL DETAIL --}}
                     <div class="modal fade" id="modalDetailKategori" tabindex="-1"
                         aria-labelledby="modalDetailKategoriLabel" aria-hidden="true">
@@ -138,5 +124,8 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    </div>
     </div>
 @endsection
