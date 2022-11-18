122c7bf30d29501240cdf3251fca521721fc135f
icang02 <ilmifaizan1112@gmail.com>
2022-11-18 20:47:44 +0800

    update hoome jumbotron

--- resources/views/Home/components/jumbotron.blade.php
+++ resources/views/Home/components/jumbotron.blade.php
@@ -1,9 +1,11 @@
 <div class="container-fluid p-0 border">
+  <img class="img-fluid d-none img1" src="{{ asset('storage/img/slider/' . 'slider1.png') }}" alt="Slider 1">
+
   <div class="owl-carousel header-carousel position-relative">
     <div class="owl-carousel-item position-relative">
       <img class="img-fluid" src="{{ asset('storage/img/slider/' . 'slider1.png') }}" alt="Slider 1">
       <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
-        style="background: rgba(24, 29, 56, .7);">
+        style="background: rgba(24, 29, 56, 0);">
         <div class="container">
           <div class="row justify-content-start">
             <div class="col-sm-10 col-lg-8">
@@ -11,8 +13,8 @@
                 style="text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
                 0px 8px 13px rgba(0,0,0,0.1),
                 0px 18px 23px rgba(0,0,0,0.1);">
-                Selamat datang di</h3>
-              <h1 class="display-3 text-white animated slideInDown">FAKULTAS HUKUM UHO</h1>
+              </h3>
+              <h1 class="display-3 text-white animated slideInDown"></h1>
             </div>
           </div>
         </div>
@@ -20,9 +22,9 @@
     </div>
 
     <div class="owl-carousel-item position-relative">
-      <img class="img-fluid" src="{{ asset('storage/img/slider/' . 'slider2.jpg') }}" alt="Slider 2">
+      <img class="img-fluid" src="{{ asset('storage/img/slider/' . 'slider2.png') }}" alt="Slider 2">
       <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
-        style="background: rgba(24, 29, 56, .7);">
+        style="background: rgba(24, 29, 56, 0);">
         <div class="container">
           <div class="row justify-content-start">
             <div class="col-sm-10 col-lg-8">
@@ -30,8 +32,8 @@
                 style="text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
               0px 8px 13px rgba(0,0,0,0.1),
               0px 18px 23px rgba(0,0,0,0.1);">
-                SITUS WEB RESMI</h3>
-              <h1 class="display-3 text-white animated slideInDown">FAKULTAS HUKUM UHO</h1>
+              </h3>
+              <h1 class="display-3 text-white animated slideInDown"></h1>
             </div>
           </div>
         </div>
@@ -39,3 +41,11 @@
     </div>
   </div>
 </div>
+
+<style>
+  @media only screen and (max-width: 600px) {
+    .img1 {
+      display: block;
+    }
+  }
+</style>
