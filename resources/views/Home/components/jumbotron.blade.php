1ea9f02737c6df6c76d90ffcc68173277ec6c0c7
Amharrayadin <amharrayadin@gmail.com>
2022-11-18 20:56:56 +0800

    jumm

--- resources/views/Home/components/jumbotron.blade.php
+++ resources/views/Home/components/jumbotron.blade.php
@@ -2,40 +2,10 @@
   <div class="owl-carousel header-carousel position-relative">
     <div class="owl-carousel-item position-relative">
       <img class="img-fluid" src="{{ asset('storage/img/slider/' . 'slider1.png') }}" alt="Slider 1">
-      <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
-        style="background: rgba(24, 29, 56, .7);">
-        <div class="container">
-          <div class="row justify-content-start">
-            <div class="col-sm-10 col-lg-8">
-              <h3 class="text-primary text-uppercase mb-3 animated slideInDown"
-                style="text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
-                0px 8px 13px rgba(0,0,0,0.1),
-                0px 18px 23px rgba(0,0,0,0.1);">
-                Selamat datang di</h3>
-              <h1 class="display-3 text-white animated slideInDown">FAKULTAS HUKUM UHO</h1>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
 
     <div class="owl-carousel-item position-relative">
-      <img class="img-fluid" src="{{ asset('storage/img/slider/' . 'slider2.jpg') }}" alt="Slider 2">
-      <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
-        style="background: rgba(24, 29, 56, .7);">
-        <div class="container">
-          <div class="row justify-content-start">
-            <div class="col-sm-10 col-lg-8">
-              <h3 class="text-primary text-uppercase mb-3 animated slideInDown"
-                style="text-shadow: 0px 4px 3px rgba(0,0,0,0.4),
-              0px 8px 13px rgba(0,0,0,0.1),
-              0px 18px 23px rgba(0,0,0,0.1);">
-                SITUS WEB RESMI</h3>
-              <h1 class="display-3 text-white animated slideInDown">FAKULTAS HUKUM UHO</h1>
-            </div>
-          </div>
-        </div>
-      </div>
+      <img class="img-fluid" src="{{ asset('storage/img/slider/' . 'slider2.png') }}" alt="Slider 2">
     </div>
   </div>
 </div>
